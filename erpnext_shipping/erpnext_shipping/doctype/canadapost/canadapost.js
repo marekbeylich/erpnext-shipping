4d2b0d20a5bf6096914c45a285baa9367f4591a9
marekbeylich <marekbey@videotron.ca>
2022-10-28 20:17:51 -0400

    Fixed Naming issue

--- erpnext_shipping/erpnext_shipping/doctype/canadapost/canadapost.js
+++ erpnext_shipping/erpnext_shipping/doctype/canadapost/canadapost.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022, Frappe and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Canada Post', {
+frappe.ui.form.on('CanadaPost', {
 	// refresh: function(frm) {
 
 	// }
